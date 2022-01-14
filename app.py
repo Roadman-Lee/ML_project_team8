4ef99c036a68aacff16dc3517e96c48aee28f0b2
GrolarKim <audwnswmf@naver.com>
2022-01-14 21:04:33 +0900

    모델변경

--- app.py
+++ app.py
@@ -14,10 +14,8 @@ from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications.imagenet_utils import preprocess_input
 
-from datetime import datetime
 
-
-MODEL_PATH = 'static/model/pokemon_ResNet50V2_03_model.h5'
+MODEL_PATH = 'static/model/pokemon_ResNet50V2_04_model.h5'
 model = load_model(MODEL_PATH)
 
 print('Model loaded. Start serving...')
@@ -38,9 +36,152 @@ def model_predict(img, model):
 def pred_result(image):
     #테스트 시 사용했던 클래스라벨
     # 7000라벨 포켓몬 https://www.kaggle.com/lantian773030/pokemonclassification
-    test_class_indices = {'Abra': 0, 'Aerodactyl': 1, 'Alakazam': 2, 'Alolan Sandslash': 3, 'Arbok': 4, 'Arcanine': 5, 'Articuno': 6, 'Beedrill': 7, 'Bellsprout': 8, 'Blastoise': 9, 'Bulbasaur': 10, 'Butterfree': 11, 'Caterpie': 12, 'Chansey': 13, 'Charizard': 14, 'Charmander': 15, 'Charmeleon': 16, 'Clefable': 17, 'Clefairy': 18, 'Cloyster': 19, 'Cubone': 20, 'Dewgong': 21, 'Diglett': 22, 'Ditto': 23, 'Dodrio': 24, 'Doduo': 25, 'Dragonair': 26, 'Dragonite': 27, 'Dratini': 28, 'Drowzee': 29, 'Dugtrio': 30, 'Eevee': 31, 'Ekans': 32, 'Electabuzz': 33, 'Electrode': 34, 'Exeggcute': 35, 'Exeggutor': 36, 'Farfetchd': 37, 'Fearow': 38, 'Flareon': 39, 'Gastly': 40, 'Gengar': 41, 'Geodude': 42, 'Gloom': 43, 'Golbat': 44, 'Goldeen': 45, 'Golduck': 46, 'Golem': 47, 'Graveler': 48, 'Grimer': 49, 'Growlithe': 50, 'Gyarados': 51, 'Haunter': 52, 'Hitmonchan': 53, 'Hitmonlee': 54, 'Horsea': 55, 'Hypno': 56, 'Ivysaur': 57, 'Jigglypuff': 58, 'Jolteon': 59, 'Jynx': 60, 'Kabuto': 61, 'Kabutops': 62, 'Kadabra': 63, 'Kakuna': 64, 'Kangaskhan': 65, 'Kingler': 66, 'Koffing': 67, 'Krabby': 68, 'Lapras': 69, 'Lickitung': 70, 'Machamp': 71, 'Machoke': 72, 'Machop': 73, 'Magikarp': 74, 'Magmar': 75, 'Magnemite': 76, 'Magneton': 77, 'Mankey': 78, 'Marowak': 79, 'Meowth': 80, 'Metapod': 81, 'Mew': 82, 'Mewtwo': 83, 'Moltres': 84, 'MrMime': 85, 'Muk': 86, 'Nidoking': 87, 'Nidoqueen': 88, 'Nidorina': 89, 'Nidorino': 90, 'Ninetales': 91, 'Oddish': 92, 'Omanyte': 93, 'Omastar': 94, 'Onix': 95, 'Paras': 96, 'Parasect': 97, 'Persian': 98, 'Pidgeot': 99, 'Pidgeotto': 100, 'Pidgey': 101, 'Pikachu': 102, 'Pinsir': 103, 'Poliwag': 104, 'Poliwhirl': 105, 'Poliwrath': 106, 'Ponyta': 107, 'Porygon': 108, 'Primeape': 109, 'Psyduck': 110, 'Raichu': 111, 'Rapidash': 112, 'Raticate': 113, 'Rattata': 114, 'Rhydon': 115, 'Rhyhorn': 116, 'Sandshrew': 117, 'Sandslash': 118, 'Scyther': 119, 'Seadra': 120, 'Seaking': 121, 'Seel': 122, 'Shellder': 123, 'Slowbro': 124, 'Slowpoke': 125, 'Snorlax': 126, 'Spearow': 127, 'Squirtle': 128, 'Starmie': 129, 'Staryu': 130, 'Tangela': 131, 'Tauros': 132, 'Tentacool': 133, 'Tentacruel': 134, 'Vaporeon': 135, 'Venomoth': 136, 'Venonat': 137, 'Venusaur': 138, 'Victreebel': 139, 'Vileplume': 140, 'Voltorb': 141, 'Vulpix': 142, 'Wartortle': 143, 'Weedle': 144, 'Weepinbell': 145, 'Weezing': 146, 'Wigglytuff': 147, 'Zapdos': 148, 'Zubat': 149}
+    test_class_indices = {'Abra': 0, 
+    'Aerodactyl': 1,
+    'Alakazam': 2,
+    'Arbok': 3,
+    'Arcanine': 4,
+    'Articuno': 5,
+    'Beedrill': 6,
+    'Bellsprout': 7,
+    'Blastoise': 8,
+    'Bulbasaur': 9,
+    'Butterfree': 10,
+    'Caterpie': 11,
+    'Chansey': 12,
+    'Charizard': 13,
+    'Charmander': 14,
+    'Charmeleon': 15,
+    'Clefable': 16,
+    'Clefairy': 17,
+    'Cloyster': 18,
+    'Cubone': 19,
+    'Dewgong': 20,
+    'Diglett': 21,
+    'Ditto': 22,
+    'Dodrio': 23,
+    'Doduo': 24,
+    'Dragonair': 25,
+    'Dragonite': 26,
+    'Dratini': 27,
+    'Drowzee': 28,
+    'Dugtrio': 29,
+    'Eevee': 30,
+    'Ekans': 31,
+    'Electabuzz': 32,
+    'Electrode': 33,
+    'Exeggcute': 34,
+    'Exeggutor': 35,
+    'Farfetchd': 36,
+    'Fearow': 37,
+    'Flareon': 38,
+    'Gastly': 39,
+    'Gengar': 40,
+    'Geodude': 41,
+    'Gloom': 42,
+    'Golbat': 43,
+    'Goldeen': 44,
+    'Golduck': 45,
+    'Graveler': 46,
+    'Grimer': 47,
+    'Growlithe': 48,
+    'Gyarados': 49,
+    'Haunter': 50,
+    'Hitmonchan': 51,
+    'Hitmonlee': 52,
+    'Horsea': 53,
+    'Hypno': 54,
+    'Ivysaur': 55,
+    'Jigglypuff': 56,
+    'Jolteon': 57,
+    'Jynx': 58,
+    'Kabutops': 59,
+    'Kadabra': 60,
+    'Kakuna': 61,
+    'Kangaskhan': 62,
+    'Kingler': 63,
+    'Koffing': 64,
+    'Lapras': 65,
+    'Lickitung': 66,
+    'Machamp': 67,
+    'Machoke': 68,
+    'Machop': 69,
+    'Magikarp': 70,
+    'Magmar': 71,
+    'Magnemite': 72,
+    'Magneton': 73,
+    'Mankey': 74,
+    'Marowak': 75,
+    'Meowth': 76,
+    'Metapod': 77,
+    'Mew': 78,
+    'Mewtwo': 79,
+    'Moltres': 80,
+    'Mr. Mime': 81,
+    'MrMime': 82,
+    'Nidoking': 83,
+    'Nidoqueen': 84,
+    'Nidorina': 85,
+    'Nidorino': 86,
+    'Ninetales': 87,
+    'Oddish': 88,
+    'Omanyte': 89,
+    'Omastar': 90,
+    'Parasect': 91,
+    'Pidgeot': 92,
+    'Pidgeotto': 93,
+    'Pidgey': 94,
+    'Pikachu': 95,
+    'Pinsir': 96,
+    'Poliwag': 97,
+    'Poliwhirl': 98,
+    'Poliwrath': 99,
+    'Ponyta': 100,
+    'Porygon': 101,
+    'Primeape': 102,
+    'Psyduck': 103,
+    'Raichu': 104,
+    'Rapidash': 105,
+    'Raticate': 106,
+    'Rattata': 107,
+    'Rhydon': 108,
+    'Rhyhorn': 109,
+    'Sandshrew': 110,
+    'Sandslash': 111,
+    'Scyther': 112,
+    'Seadra': 113,
+    'Seaking': 114,
+    'Seel': 115,
+    'Shellder': 116,
+    'Slowbro': 117,
+    'Slowpoke': 118,
+    'Snorlax': 119,
+    'Spearow': 120,
+    'Squirtle': 121,
+    'Starmie': 122,
+    'Staryu': 123,
+    'Tangela': 124,
+    'Tauros': 125,
+    'Tentacool': 126,
+    'Tentacruel': 127,
+    'Vaporeon': 128,
+    'Venomoth': 129,
+    'Venonat': 130,
+    'Venusaur': 131,
+    'Victreebel': 132,
+    'Vileplume': 133,
+    'Voltorb': 134,
+    'Vulpix': 135,
+    'Wartortle': 136,
+    'Weedle': 137,
+    'Weepinbell': 138,
+    'Weezing': 139,
+    'Wigglytuff': 140,
+    'Zapdos': 141,
+    'Zubat': 142}
     # 1세대포켓몬 https://www.kaggle.com/thedagger/pokemon-generation-one
-    test_class_indices2 = {'Abra': 0, 'Aerodactyl': 1, 'Alakazam': 2, 'Arbok': 3, 'Arcanine': 4, 'Articuno': 5, 'Beedrill': 6, 'Bellsprout': 7, 'Blastoise': 8, 'Bulbasaur': 9, 'Butterfree': 10, 'Caterpie': 11, 'Chansey': 12, 'Charizard': 13, 'Charmander': 14, 'Charmeleon': 15, 'Clefable': 16, 'Clefairy': 17, 'Cloyster': 18, 'Cubone': 19, 'Dewgong': 20, 'Diglett': 21, 'Ditto': 22, 'Dodrio': 23, 'Doduo': 24, 'Dragonair': 25, 'Dragonite': 26, 'Dratini': 27, 'Drowzee': 28, 'Dugtrio': 29, 'Eevee': 30, 'Ekans': 31, 'Electabuzz': 32, 'Electrode': 33, 'Exeggcute': 34, 'Exeggutor': 35, 'Farfetchd': 36, 'Fearow': 37, 'Flareon': 38, 'Gastly': 39, 'Gengar': 40, 'Geodude': 41, 'Gloom': 42, 'Golbat': 43, 'Goldeen': 44, 'Golduck': 45, 'Golem': 46, 'Graveler': 47, 'Grimer': 48, 'Growlithe': 49, 'Gyarados': 50, 'Haunter': 51, 'Hitmonchan': 52, 'Hitmonlee': 53, 'Horsea': 54, 'Hypno': 55, 'Ivysaur': 56, 'Jigglypuff': 57, 'Jolteon': 58, 'Jynx': 59, 'Kabuto': 60, 'Kabutops': 61, 'Kadabra': 62, 'Kakuna': 63, 'Kangaskhan': 64, 'Kingler': 65, 'Koffing': 66, 'Krabby': 67, 'Lapras': 68, 'Lickitung': 69, 'Machamp': 70, 'Machoke': 71, 'Machop': 72, 'Magikarp': 73, 'Magmar': 74, 'Magnemite': 75, 'Magneton': 76, 'Mankey': 77, 'Marowak': 78, 'Meowth': 79, 'Metapod': 80, 'Mew': 81, 'Mewtwo': 82, 'Moltres': 83, 'MrMime': 84, 'Muk': 85, 'Nidoking': 86, 'Nidoqueen': 87, 'Nidorina': 88, 'Nidorino': 89, 'Ninetales': 90, 'Oddish': 91, 'Omanyte': 92, 'Omastar': 93, 'Onix': 94, 'Paras': 95, 'Parasect': 96, 'Persian': 97, 'Pidgeot': 98, 'Pidgeotto': 99, 'Pidgey': 100, 'Pikachu': 101, 'Pinsir': 102, 'Poliwag': 103, 'Poliwhirl': 104, 'Poliwrath': 105, 'Ponyta': 106, 'Porygon': 107, 'Primeape': 108, 'Psyduck': 109, 'Raichu': 110, 'Rapidash': 111, 'Raticate': 112, 'Rattata': 113, 'Rhydon': 114, 'Rhyhorn': 115, 'Sandshrew': 116, 'Sandslash': 117, 'Scyther': 118, 'Seadra': 119, 'Seaking': 120,  'Seel': 121, 'Shellder': 122, 'Slowbro': 123, 'Slowpoke': 124, 'Snorlax': 125, 'Spearow': 126, 'Squirtle': 127, 'Starmie': 128, 'Staryu': 129, 'Tangela': 130, 'Tauros': 131, 'Tentacool': 132, 'Tentacruel': 133, 'Vaporeon': 134, 'Venomoth': 135, 'Venonat': 136, 'Venusaur': 137, 'Victreebel': 138, 'Vileplume': 139, 'Voltorb': 140, 'Vulpix': 141, 'Wartortle': 142, 'Weedle': 143, 'Weepinbell': 144, 'Weezing': 145, 'Wigglytuff': 146, 'Zapdos': 147, 'Zubat': 148, 'dataset': 149}    
+    # 1세대포켓몬 https://www.kaggle.com/mikoajkolman/pokemon-images-first-generation17000-files
+    
     
     #클래스 라벨의 키와 밸류 반대로
     classes = dict((v, k) for k, v in test_class_indices.items())
@@ -53,8 +194,7 @@ def pred_result(image):
     pred_label = classes[np.argmax(preds)]
     #예측결과 클래스와 확률 출력
     print(pred_label, pred_proba)
-    if pred_label == 'Alolan Sandslash':
-        return 'Sandslash'
+    
     return pred_label
 
 
