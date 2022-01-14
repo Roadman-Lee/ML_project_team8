f850ce1b6c3df9708b04caab27829b735631f50d
gaeul <e0540@hanmail.net>
2022-01-14 16:54:30 +0900

    메세지박스 누르면  두번째이미지 올라오도록 변경

--- static/js/upload.js
+++ static/js/upload.js
@@ -18,3 +18,29 @@ function handleImgFileSelect(e) {
         reader.readAsDataURL(f);
     });
 }
+function box_change(){
+    var wrap = document.getElementById('pop');
+           wrap.style.zIndex = '3';
+}
+function posting_img() {
+    let d = new Date()
+    let time = d.getTime()
+    let title = String(time)
+    let file = $('#input_img')[0].files[0]
+    let form_data = new FormData()
+    form_data.append("file_give", file)
+    form_data.append("title_give", title)
+
+    $.ajax({
+        type: "POST",
+        url: "/api/upload/",
+        data: form_data,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            alert(response["msg"])
+            window.location.href = '/pokedex/' + title
+        }
+    });
+}
